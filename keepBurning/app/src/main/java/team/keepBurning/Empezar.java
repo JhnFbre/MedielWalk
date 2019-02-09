34b1fe5b8a2ede46ce506c52b5eb728045ba6a33
kehylaicaza <keilacicaza@gmail.com>
2019-02-09 13:03:01 -0500

    validacion prueba

--- keepBurning/app/src/main/java/team/keepBurning/Empezar.java
+++ keepBurning/app/src/main/java/team/keepBurning/Empezar.java
@@ -1,19 +1,29 @@
 package team.keepBurning;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Empezar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.empezar);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.empezar);
+
 
     }
+
+
     public void logear(View v){
         Intent intent= new Intent(this, Login.class);
         startActivity(intent);
--- keepBurning/app/src/main/java/team/keepBurning/SplashScreen.java
+++ keepBurning/app/src/main/java/team/keepBurning/SplashScreen.java
@@ -2,30 +2,69 @@ package team.keepBurning;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashScreen extends Activity {
     private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
-        logo = (ImageView) findViewById(R.id.logo);
-        Animation anima = AnimationUtils.loadAnimation(this, R.anim.fadein);
-        logo.startAnimation(anima);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent= new Intent(SplashScreen.this, Empezar.class);
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash_screen);
+
+            logo = (ImageView) findViewById(R.id.logo);
+            Animation anima = AnimationUtils.loadAnimation(this, R.anim.fadein);
+            logo.startAnimation(anima);
+        printKeyHash();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+              validar();
+                }
+            },2500);
+
+
+
+        }
+
+        public void validar(){
+            String sesioniciada=getFromSharedPreferences("usuario");
+                    /*Intent intent= new Intent(SplashScreen.this, Empezar.class);
+                    startActivity(intent);*/
+            if (sesioniciada!=""){
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
+            }else{
+                Intent intent  = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent );
             }
-        },2500);
+            finish();
+        }
+    private void printKeyHash(){
+
     }
+    private String getFromSharedPreferences(String usuario) {
+        SharedPreferences sharedPre = getSharedPreferences("login_preference",Context.MODE_PRIVATE);
+        return  sharedPre.getString(usuario,"");
+
+    }
+
+
+
+
+
+
+
 }
